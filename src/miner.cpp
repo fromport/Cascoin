a81b3ada1d37283c1715b223ca3b78055f0b5362
Casraw <casraw@protonmail.com>
2025-05-12 13:30:36 +0200

    Revert "maybe revert"
    This reverts commit 9c667d5765625b16107485c3cf4e5549096b4eb9.

--- src/miner.cpp
+++ src/miner.cpp
@@ -242,7 +242,7 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
 
         coinbaseTx.vout[0].nValue += nFees;
 
-        coinbaseTx.vin[0].scriptSig = CScript() << CScriptNum(nHeight);
+        coinbaseTx.vin[0].scriptSig = CScript() << nHeight << OP_0;
         pblock->vtx[0] = MakeTransactionRef(std::move(coinbaseTx));
         pblocktemplate->vchCoinbaseCommitment = GenerateCoinbaseCommitment(*pblock, pindexPrev, chainparams.GetConsensus());
         pblocktemplate->vTxFees[0] = -nFees;
