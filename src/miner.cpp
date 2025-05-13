b981d4274986f38b8799583fc9d3423a17701cf3
Casraw <casraw@protonmail.com>
2025-05-13 08:12:05 +0200

    Revert "fixing lock"
    This reverts commit 4507ed9bf520795123ac8a2c21ec889c9280f8e5.

--- src/miner.cpp
+++ src/miner.cpp
@@ -788,9 +788,6 @@ bool BusyBees(const Consensus::Params& consensusParams, int height) {
 
     LogPrintf("********************* Hive: Bees at work *********************\n");
 
-    // We need to hold cs_main for all blockchain operations
-    LOCK(cs_main);
-    
     // Find deterministicRandString
     std::string deterministicRandString = GetDeterministicRandString(pindexPrev);
     if (verbose) LogPrintf("BusyBees: deterministicRandString   = %s\n", deterministicRandString);
@@ -800,7 +797,7 @@ bool BusyBees(const Consensus::Params& consensusParams, int height) {
     beeHashTarget.SetCompact(GetNextHiveWorkRequired(pindexPrev, consensusParams));
     if (verbose) LogPrintf("BusyBees: beeHashTarget             = %s\n", beeHashTarget.ToString());
 
-    // Find bin size - The wallet functions need cs_main for blockchain access
+    // Find bin size
     std::vector<CBeeCreationTransactionInfo> potentialBcts = pwallet->GetBCTs(false, false, consensusParams);
     std::vector<CBeeCreationTransactionInfo> bcts;
     int totalBees = 0;
