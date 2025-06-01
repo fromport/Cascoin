c1e246537b80ae829c8aec88637efca65e05fd64
Casraw <casraw@protonmail.com>
2025-06-01 15:48:28 +0200

    fixing tabs

--- depends/funcs.mk
+++ depends/funcs.mk
@@ -20,17 +20,17 @@ $(sort $(foreach dep,$(2),$(2) $(call int_get_all_dependencies,$(1),$($(dep)_dep
 endef
 
 define fetch_file_inner
-    ( mkdir -p $$($(1)_download_dir) && echo Fetching $(3) from $(2) && \
-    $(build_DOWNLOAD) "$$($(1)_download_dir)/$(4).temp" "$(2)/$(3)" && \
-    echo "$(5)  $$($(1)_download_dir)/$(4).temp" > $$($(1)_download_dir)/.$(4).hash && \
-    $(build_SHA256SUM) -c $$($(1)_download_dir)/.$(4).hash && \
-    mv $$($(1)_download_dir)/$(4).temp $$($(1)_source_dir)/$(4) && \
+    ( mkdir -p $$($(1)_download_dir) && echo Fetching $(3) from $(2) && \\
+    $(build_DOWNLOAD) "$$($(1)_download_dir)/$(4).temp" "$(2)/$(3)" && \\
+    echo "$(5)  $$($(1)_download_dir)/$(4).temp" > $$($(1)_download_dir)/.$(4).hash && \\
+    $(build_SHA256SUM) -c $$($(1)_download_dir)/.$(4).hash && \\
+    mv $$($(1)_download_dir)/$(4).temp $$($(1)_source_dir)/$(4) && \\
     rm -rf $$($(1)_download_dir) )
 endef
 
 define fetch_file
-    ( test -f $$($(1)_source_dir)/$(4) || \
-    ( $(call fetch_file_inner,$(1),$(2),$(3),$(4),$(5)) || \
+    ( test -f $$($(1)_source_dir)/$(4) || \\
+    ( $(call fetch_file_inner,$(1),$(2),$(3),$(4),$(5)) || \\
       $(call fetch_file_inner,$(1),$(FALLBACK_DOWNLOAD_PATH),$(3),$(4),$(5))))
 endef
 
